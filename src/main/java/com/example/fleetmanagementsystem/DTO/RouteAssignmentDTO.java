1a5818c51b4edec6672431650973f05e0b27ecad
Yvette Njeri <yvettenjeri79@gmail.com>
2025-07-28 14:40:15 +0300

    Route assignment and unassignment DTOs

---
+++ src/main/java/com/example/fleetmanagementsystem/DTO/RouteAssignmentDTO.java
@@ -0,0 +1,10 @@
+package com.example.fleetmanagementsystem.DTO;
+
+import lombok.Data;
+
+@Data
+public class RouteAssignmentDTO {
+    private Long routeId;
+    private Long marshallId;
+    private String position;
+}
---
+++ src/main/java/com/example/fleetmanagementsystem/DTO/RouteUnassignmentDTO.java
@@ -0,0 +1,8 @@
+package com.example.fleetmanagementsystem.DTO;
+
+import lombok.Data;
+
+@Data
+public class RouteUnassignmentDTO {
+    private Long MarshallId;
+}
